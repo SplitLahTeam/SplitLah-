1d007e9a3e2ce1bf8caf7995740837f91a56069c
antoponselvan <anto.ponselvan@gmail.com>
2022-10-28 14:45:49 +0800

    Added route for group creation

--- Server/controllers/groupController.js
+++ Server/controllers/groupController.js
@@ -5,7 +5,31 @@ const Transaction = require("../models/transaction")
 
 const registerGroup = (req, res) => {
 // req should contain - All schema entries of "Group" collection
-    
+const name = req.body.name
+const userList = req.body.userList
+if ((!name)||(!userList)) {
+    res.status(400).json({msg: "Inadequate Details to register a group"})
+    return
+}
+
+let invalidUserId = false
+userList.foreach((userId)=>{
+    const user = User.findById(userId)
+    if (!user) {
+        res.status(400).json({msg: "Inaccurate user ID details"})
+        invalidUserId = true
+        return
+    }
+})
+if (invalidUserId) {return}
+
+try {
+    const newGroup = Group.create({name, userList})
+    res.status(201).json({msg:"New group registed",
+    name: newGroup.name})
+} catch (error) {
+    res.status(500).json({msg: "Some Server Error"})
+}
 }
 
 const updateGroup = (req, res) => {
