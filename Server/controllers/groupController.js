ac19c70ed9a20d677b28401723a56f51c692b30f
antoponselvan <anto.ponselvan@gmail.com>
2022-10-28 15:42:07 +0800

    Completed route for updating group

--- Server/controllers/groupController.js
+++ Server/controllers/groupController.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 const Group = require("../models/group")
 const Transaction = require("../models/transaction")
 
-const registerGroup = (req, res) => {
+const registerGroup = async (req, res) => {
 // req should contain - All schema entries of "Group" collection
 const name = req.body.name
 const userList = req.body.userList
@@ -13,8 +13,8 @@ if ((!name)||(!userList)) {
 }
 
 let invalidUserId = false
-userList.forEach((userId)=>{
-    const user = User.findById(userId)
+userList.forEach(async (userId)=>{
+    const user = await User.findById(userId)
     if (!user) {
         res.status(400).json({msg: "Inaccurate user ID details"})
         invalidUserId = true
@@ -24,7 +24,7 @@ userList.forEach((userId)=>{
 if (invalidUserId) {return}
 
 try {
-    const newGroup = Group.create({name, userList})
+    const newGroup = await Group.create({name, userList})
     res.status(201).json({msg:"New group registed",
     name: newGroup.name})
 } catch (error) {
@@ -33,26 +33,55 @@ try {
 }
 
 
-
-const updateGroup = (req, res) => {
+const updateGroup = async (req, res) => {
 // req should contain - All schema entries of "Group" collection
-    const id = req.body.id
+    const id = req.body.id;
     const name = req.body.name
     const userList = req.body.userList
 
-    if ((!id)||(!name)||(!userList)) {
-        res.status(400).json({
-            msg: ""
-        })
+    try {
+
+        if ((!id)||(!name)||(!userList)) {
+            res.status(400).json({
+                msg: "Inadequate Details to update group"
+            })
+            return
+        }
+        const group = await Group.findById(id)
+        if (!group){
+            res.status(400).json({
+                msg: "No group found for given ID"
+            })
+            return
+        }
+        const invalidUserId = false
+        userList.forEach((userId) => {
+            const user = User.findById(userId)
+            if (!user){
+                invalidUserId = true
+                res.status(400).json({
+                    msg: "Inaccurate user ID details"
+                })
+                return
+            }
+        }) 
+        if (invalidUserId) {return}
+        
+        group.name = name
+        group.userList = userList
+        await group.save()
+        res.status(202).json({msg: "Group Updated"})
+    } catch (error) {
+        res.status(500).json({msg: "Server Error"})
     }
 }
 
-const getGroupsSummary = (req, res) => {
+const getGroupsSummary = async (req, res) => {
 // req should contain - UserID
 
 }
 
-const getGroupDetails = (req,res) => {
+const getGroupDetails = async (req,res) => {
 // req should contain - UserID and GroupID
 
 }
