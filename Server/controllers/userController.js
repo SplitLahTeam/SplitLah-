3341b81d31e3eb4a43bbbc65316eb63e57a4dc4e
antoponselvan <anto.ponselvan@gmail.com>
2022-10-28 12:31:47 +0800

    Completed User login & logOut functions

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -6,12 +6,52 @@ const registerUser = (req,res)=>{
 // req should contain - All schema entries of "User" collection
 }
 
-const loginUser = (req, res)=>{
+
+const loginUser = async (req, res)=>{
 // req should contain - email and password
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email) {
+        res.status(400).json({msg: "Login Email-ID cannot be blank"})
+        return
+    }
+
+    try{
+        const user = await User.findOne({email}).exec()
+        if (!user) {
+            res.status(404).json({msg: "User not found"})
+            return
+        }
+
+        const loginPass = bcrypt.compareSync(password, user.password)
+        if (loginPass){
+            req.session.userId = user._id
+            res.status(202).json({msg: "Successful Login"})
+        } else {
+            req.sesssion.userId = null
+            res.status(401).json({msg: "Inaccurate Password"})
+        }
+    } catch (error) {
+        res.status(500).json({msg: "Unknown Server Error"})
+    }
 }
+
 
 const updateUser = (req, res) => {
 // req should contain - All schema entries of "User" collection
+
 }
 
-module.exports = {registerUser, loginUser, updateUser}
+
+const logoutUser = (req, res) => {
+    if (!req.session.userId){
+        res.status(400).json({msg: "No user to log-out"})
+        return
+    }
+    console.log(req.session.userId)
+    req.session.userId = null
+    res.status(202).json({msg: "User successfully logged out!"})
+}
+
+module.exports = {registerUser, loginUser, updateUser, logoutUser}
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const userRouter = express.Router()
-const {registerUser, loginUser, updateUser} = require("../controllers/userController")
+const {registerUser, loginUser, updateUser, logoutUser} = require("../controllers/userController")
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login',loginUser)
 userRouter.put('/', updateUser)
+userRouter.post('/logout', logoutUser)
 
 module.exports = userRouter
