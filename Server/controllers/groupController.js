b0d895e244340641ea378933024ca1fc04b494c8
antoponselvan <anto.ponselvan@gmail.com>
2022-10-28 23:11:21 +0800

    Completed routes - groupSummary & groupDetails

--- Server/controllers/groupController.js
+++ Server/controllers/groupController.js
@@ -83,7 +83,6 @@ const getTransactions = async (userId, groupId) => {
         const  receivedTransactionDocuments = await Transaction.find({receivedBy:userId, groupId:groupId})
         const receivedTransactions = {}
         receivedTransactionDocuments.forEach((transaction)=>{
-            // console.log(transaction.paidBy, transaction.amount, "AA")
             if (receivedTransactions[String(transaction.paidBy)]){
                 receivedTransactions[String(transaction.paidBy)].push(transaction.amount)
             } else {
@@ -91,13 +90,10 @@ const getTransactions = async (userId, groupId) => {
             }
         })
         transactions.recevedAmounts = receivedTransactions
-        // console.log("RT :", receivedTransactions)
-        // return receivedTransactions
         
         const paidTransactionsDocuments = await Transaction.find({paidBy:userId, groupId:groupId})
         const paidTransactions = {}
         paidTransactionsDocuments.forEach((transaction)=>{
-            // console.log(transaction.paidBy, transaction.amount, "AA")
             if (paidTransactions[String(transaction.receivedBy)]){
                 paidTransactions[String(transaction.receivedBy)].push(transaction.amount)
             } else {
@@ -113,43 +109,70 @@ const getTransactions = async (userId, groupId) => {
 
 const getGroupsSummary = async (req, res) => {
 // req should contain - UserID
-    const userId = req.body.userId
-    if (!userId){
-        res.status(400).json({msg: "No UserID found"})
-        return
-    }
-    const user = await User.findById(userId)
-    if (!user){
-        res.status(400).json({msg: "Invalid UserID"})
-        return
-    }
-    const groups = await Group.find({
-        userList : { $all : [userId]}
-    })
-    // const groupSummaryList = groups.map(async (group)=> {
-    //     const transactions = await getTransactions(userId, group._id)
-        // const netReceivedAmount = transactions.receivedTransactions.reduce((prev,current)=> (prev+current),0)
-        // const netPaidAmount = transactions.paidTransactions.reduce((prev, curr) => (prev+curr),0)
-        // return {id: group._id, name:group.name, amountToReceive:(netPaidAmount-netReceivedAmount)}
-    //     return transactions
-    // })
-    let groupSummaryList = []
-    for (group of groups){
-        const transactions = await getTransactions(userId, group._id)
-        const groupSummary = {}
-        groupSummary.id = group._id
-        groupSummary.name = group.name
-        groupSummary.transactions = transactions
-        groupSummaryList.push(groupSummary)
-    }
+    try {
+        const userId = req.body.userId
+        if (!userId){
+            res.status(400).json({msg: "No UserID found"})
+            return
+        }
+        const user = await User.findById(userId)
+        if (!user){
+            res.status(400).json({msg: "Invalid UserID"})
+            return
+        }
+        const groups = await Group.find({
+            userList : { $all : [userId]}
+        })
+        const groupsSummaryList = []
+        for (const group of groups){
+            const transactions = await getTransactions(userId, group._id)
+            const groupSummary = {}
+            groupSummary.id = group._id
+            groupSummary.name = group.name
 
-    console.log("GS", groupSummaryList)
-    res.status(200).json(groupSummaryList)
+            let [amountReceived, amountPaid] = [0,0]
+            for (const user in transactions.receivedTransactions){
+                amountReceived = amountReceived + transactions.receivedTransactions[user].reduce((prev,curr)=>(prev+curr),0)
+            }
+            for (const user in transactions.paidTransactions){
+                amountPaid = amountPaid + transactions.paidTransactions[user].reduce((prev,curr)=>(prev+curr),0)
+            }
+            groupSummary.amountToReceive = amountPaid - amountReceived
+            groupsSummaryList.push(groupSummary)
+        }
+        res.status(200).json(groupsSummaryList)
+    } catch (error){
+        res.status(500).json({msg: "Server Error"})
+    }
 }
 
 const getGroupDetails = async (req,res) => {
 // req should contain - UserID and GroupID
+    try{
+        const userId = req.body.userId
+        const groupId = req.body.groupId
+        if (!groupId) {
+            res.status(400).json({msg: "No group ID found"})
+        }
+    
+        const transactions = await getTransactions(userId, groupId)
+        const users = [... new Set(Object.keys(transactions.receivedTransactions).concat(Object.keys(transactions.paidTransactions)))]
+        const groupDetails = []
+    
+        for (const user of users){
+            const userDetails = {}
+            userDetails.id = user
+            userDetails.name = (await User.findById(user)).name
+            const amountReceived = transactions.receivedTransactions[user]?.reduce((prev,curr)=>(prev+curr),0) || 0
+            const amoutPaid = transactions.paidTransactions[user]?.reduce((prev,curr)=>(prev+curr),0) || 0
+            userDetails.amountToRecieve = amoutPaid-amountReceived
+            groupDetails.push(userDetails)
+        }
+        res.status(200).json(groupDetails)
 
+    } catch (error) {
+        res.status(500).json({msg: "Server Error"})
+    }
 }
 
 module.exports = {registerGroup, updateGroup, getGroupsSummary, getGroupDetails}
