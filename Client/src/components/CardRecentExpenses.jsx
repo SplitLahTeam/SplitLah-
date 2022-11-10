ffd9c510dacea168aeea512528d57545ee320391
antoponselvan <anto.ponselvan@gmail.com>
2022-11-10 23:57:04 +0800

    Completed Date Time Feature

--- Client/src/components/CardRecentExpenses.jsx
+++ Client/src/components/CardRecentExpenses.jsx
@@ -1,15 +1,21 @@
 import Accordion from 'react-bootstrap/Accordion';
+// import Moment from 'react-moment'
+import moment from 'moment'
 
 const CardRecentExpenses = ({expenseTitle, transactionList}) => {
-  // console.log(transactionList)
+  // console.log(transactionList[0].updatedAt)
+  // const dateToFormat = new Date(transaction.updatedAt)
+  console.log(moment.unix(Date.parse(transactionList[0].updatedAt)/1000).format("MM/DD/YYYY HH:mm"))
+  // console.log(moment(transactionList[0].updatedAt, "YYYYMMDD HH"))
   return (
     <div>
     <Accordion defaultActiveKey="0" style={{ width: '24rem' }} >
       <Accordion.Item eventKey="0">
         <Accordion.Header>{expenseTitle}</Accordion.Header>
         <Accordion.Body>
-            <ul>{transactionList.map((transaction) =>
-              <li key={transaction._id}>{transaction.paidByName || transaction.receivedByName} on {transaction.updatedAt} ({transaction.description}) ${transaction.amount}</li>
+            <ul>
+              {transactionList.map((transaction,idx) =>
+              <li key={idx}>{transaction.paidByName || transaction.receivedByName} on {moment.unix(Date.parse(transaction.updatedAt)/1000).format("DD-MMM-YY")} ({transaction.description}) ${transaction.amount}</li>
             )}
             </ul>
         </Accordion.Body>
