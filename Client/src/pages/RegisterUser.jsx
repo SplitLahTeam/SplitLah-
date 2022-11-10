c17c851efb49309cba82104421de266fac5335b3
Anam Musta'ein <anammustaein@gmail.com>
2022-11-10 21:03:27 +0800

    Completed RegisterUser

--- Client/src/pages/RegisterUser.jsx
+++ Client/src/pages/RegisterUser.jsx
@@ -1,11 +1,33 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import {} from '@reduxjs/toolkit'
+import {useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {userActions} from '../store/userSlice'
+
 
 const RegisterUser = () => {
-
+  const dispatch = useDispatch()
   const navigate = useNavigate()
+  const userId = useSelector((state)=>state.user.id)
+
+  useEffect(()=>{
+    if (userId === ""){
+      fetch('/api/users/checklogin').then((res)=>{
+        if (res.status === 200){return res.json()}
+        throw new Error})
+        .then((data)=>{
+          dispatch(userActions.updateLoggedInUser(data))
+          navigate('/detailedpages/user/home')
+          })
+        .catch((error)=>{
+          console.log(error)
+        })
+    } else {
+      navigate("/detailedpages/user/home")
+    }
+  },[])
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const name = event.target.name.value;
@@ -13,6 +35,28 @@ const RegisterUser = () => {
     const password = event.target.password.value
 
     console.log(name, email,password)
+
+    fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    })
+      .then((res) => {
+        if (res.status !== 201) {
+          throw new Error({ msg: "Couldn't create user" })
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("Data in Comp: ", data)
+        dispatch(userActions.updateLoggedInUser(data))
+        navigate('/detailedpages/user/home')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -20,15 +64,15 @@ const RegisterUser = () => {
       <h2>Sign Up</h2>
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" name="name" placeholder="Enter name" />
+        <Form.Control type="text" name="name" placeholder="Enter name" required />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" placeholder="Enter email" />
+        <Form.Control type="email" name="email" placeholder="Enter email" required />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" placeholder="Password" />
+        <Form.Control type="password" name="password" placeholder="Enter password" required />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
       </Form.Group>
